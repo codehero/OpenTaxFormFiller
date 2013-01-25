ac4d22afe8382755c8bb565045f5472dc017228d
Name test <testmail@mailtest.cc>
2013-01-25 12:18:01 -0500

    Add new Script compare

--- script/util/checkDiff.js
+++ script/util/checkDiff.js
@@ -3,94 +3,133 @@ var util = require("util");
 var path = require("path");
 
 if(process.argc < 5){
-	throw new Error("Please specify [fieldump] [transform]");
+	throw new Error("Please specify [CMD] [YEAR] [FORM] {IGNORE yes/no}");
 }
 
 var cmd = process.argv[2];
+var year = process.argv[3];
+var form = process.argv[4];
+var ignore = process.argv[5];
 
 function fixFileJSON(buff){
 	return "{ " + buff.slice(0, buff.lastIndexOf(",")) + " }";
 }
 
-Object.prototype.size = function () {
-	var len = this.length ? --this.length : -1;
-	for (var k in this)
-		len++;
-	return len;
-}
-
-
-var pjson = JSON.parse(cmd == "dump_transform" ? fixFileJSON(fs.readFileSync(process.argv[3]).toString()) : fs.readFileSync(process.argv[3]));
-var transf = JSON.parse(fs.readFileSync(process.argv[4]));
+var fieldDumpJS = JSON.parse(fixFileJSON(fs.readFileSync(year + "/field_dump/" + form + ".pjson").toString()));
+var transfJS = JSON.parse(fs.readFileSync(year + "/transform/" + form + ".json"));
+var definitionJS = JSON.parse(fs.readFileSync(year + "/definition/" + form + ".json"));
+var supportedJS = JSON.parse(fs.readFileSync(year + "/supported.json"));
 
 result = [];
 
-if(cmd == "dump_transform"){
-for(var e in pjson){
-	var found = false;
-	for(var r in transf.fields){
-		if(pjson[e].fdf.toString() == transf.fields[r].fdf.toString() ){
-			found = true;
-			break;
-		}
-	}
-	if(found == false  /*&& pjson[e].type in ["text","checkbox"]*/ ){
-		result.push("field_dump",e, pjson[e]);
-	}
-}
-
-for(var e in transf.fields){
-	var found = false;
-	for(var r in pjson){
-		if(pjson[r].fdf == transf.fields[e].fdf){
-			found = true;
-			break;
-		}
-	}
-	if(found == false /*&& transf.fields[e].type in ["text","checkbox"]*/ ){
-		result.push("transform",e, transf.fields[e]);
-	}
-}
-}else if(cmd == "definition_transform"){
-	for(var e in pjson.fields){
-		var found = false;
-		var isArray = 0;
-		for(var r in transf.fields){
-			if(r.indexOf(e)>-1){
-				found = true;
-				if(pjson.fields[e] == "ChoiseArray"){
-					isArray = 1;
-					if(transf.fields[r].options && transf.fields[r].options.size() > 1){
-						isArray = 2;
-					}
+if(cmd == "dmp_trans"){
+/*
+ * Check FIELD DUMP file with Transform file
+ * */	
+	function checkDmpTransform(type, data1, data2){
+		firstLoop:
+		for(var e in data1){
+			for(var r in data2){
+				if((ignore == "yes" && e == r) || (data1[e].fdf == data2[r].fdf)){
+					continue firstLoop;
 				}
-				break;
+			}
+			result.push(type + " not found", e, data1[e]);
+		}
+	}
+
+	checkDmpTransform("transform", fieldDumpJS, transfJS.fields);
+	checkDmpTransform("field_dump", transfJS.fields, fieldDumpJS);
+
+}else if(cmd == "def_trans"){
+/*
+ * Check Definition file with Transform file
+ * */
+	Object.prototype.size = function () {
+		var len = this.length ? --this.length : -1;
+		for (var k in this)
+			len++;
+		return len;
+	}
+
+	//Levenshtein distance
+	function distanceLevenshtein (a, b){
+		if(a.length == 0) return b.length; 
+		if(b.length == 0) return a.length; 
+
+		var matr = [];//Matrix
+		for(var i = 0; i <= b.length; i++){
+			matr[i] = [i];
+		}
+
+		for(var j = 0; j <= a.length; j++){
+			matr[0][j] = j;
+		}
+
+		for(var i = 1; i <= b.length; i++){
+			for(var j = 1; j <= a.length; j++){
+				if(b.charAt(i-1) == a.charAt(j-1)){
+					matr[i][j] = matr[i-1][j-1];
+				} else {
+					matr[i][j] = Math.min(matr[i-1][j-1] + 1, // substitution
+					Math.min(matr[i][j-1] + 1, // insertion
+					matr[i-1][j] + 1)); // deletion
+				}
 			}
 		}
-		if(found == false || isArray == 1){
-			result.push("definition",e,pjson.fields[e], {"array" : isArray});
-		}
-	}
-	for(var e in transf.fields){
-		var found = false;
-		var isArray = 0;
-		for(var r in pjson.fields){
-			if(e.indexOf(r)>-1){
-				found = true;
-				if(transf.fields[e].options && transf.fields[e].options.size() > 0){
-					isArray = 1;
-					if(pjson.fields[r] == "ChoiseArray"){
-						isArray = 2;
-					}
+
+		return matr[b.length][a.length];
+	};
+
+	function checkDefTransform(type, data1, data2, ignoreCheck, data3){
+		firstLoop:
+		for(var e in data1.fields){
+			if(e in supportedJS.commonFields){
+				continue;
+			}
+			var compare = null;
+			var found = false;
+			var arrayProblem = false;
+			for(var r in data2.fields){
+				if(ignoreCheck === true && e in data3){
+					continue firstLoop;
 				}
-				break;
+				//look most similar word
+				if(e == "L24a" && r == "L24a_no"){
+			             var asdas=1;	
+				}
+
+				if(compare === null || (distanceLevenshtein(e, r) < distanceLevenshtein(e, compare) && (r.indexOf(e) == 0 || e.indexOf(r) == 0))){//could be better
+					compare = r;
+				}
+			}
+
+			if(compare.indexOf(e) == 0 || e.indexOf(compare) == 0){
+				found = true;
+				var existsChoiceArray = data1.fields[e] == "ChoiceArray";
+				var existsOptions = data2.fields[compare].options && data2.fields[compare].options.size() > 1;
+				if((existsOptions || existsChoiceArray) && (!existsOptions || !existsChoiceArray)){
+					arrayProblem = true;
+				}
+			}
+
+			if(found == false ){
+				result.push([type + " not found", e, data1.fields[e]]);
+			}
+			if(arrayProblem == true){
+				result.push([type + " array problem", e, data1.fields[e]]);
 			}
 		}
-		if(found == false || isArray == 1){
-			result.push("transform",e,transf.fields[e], {"array" : isArray});
-		}
 	}
+
+	checkDefTransform("definition", transfJS, definitionJS, ignore == "yes", fieldDumpJS);
+	checkDefTransform("transform", definitionJS, transfJS);
+}else{
+	throw new Error("Command not defined: " + cmd);
 }
 
+var stream = fs.createWriteStream("/tmp/checkdiff-"+ new Date().getTime() +".txt", {flags: 'w'});
 //console.error(JSON.stringify(result));
-console.error((result));
+stream.write(JSON.stringify(process.argv,null,4) + "\n");
+stream.write(JSON.stringify(result,null,4));
+//console.error((result));
